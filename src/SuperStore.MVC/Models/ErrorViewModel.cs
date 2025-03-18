ca91a63d7de87c80958e3647884c79fda1ed6c37
Tiago Henrique de Castilhos <tiago.castilhos@cyncly.com>
2025-03-18 15:27:09 -0300

    first commit

---
+++ src/SuperStore.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SuperStore.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
